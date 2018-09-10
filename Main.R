7e12659b6a11616cc3a40b75c3a8e50b2a60173e
Siamak Dehbod <dehbod@gmail.com>
2018-09-11 01:25:19 +0430

    Multivariate Stable refactor and test in kmeans

--- Main.R
+++ Main.R
@@ -9,7 +9,7 @@ source('Util.R')
 # set.seed(1038)
 cf <- list()
 cf$nSample <- 1000
-cf$nDimention <- 3
+cf$nDimention <- 2
 cf$nClass <- 2
 cf$tDimention <- 2
 
@@ -122,10 +122,13 @@ sampleDataUniStable <- function(cf, alpha) {
   
 }
 
-# d <- sampleDataUniStable(cf, 1.8)
-# print(head(data.frame(d = d$data, class = d$class)))
-# plot(d$data, type = 'p', col = d$class, asp = 1)
-# plot3d(d$data, col = d$class, size = 2, type = 's')
+if(FALSE) {
+  d <- sampleDataUniStable(cf, 1.8)
+  print(head(data.frame(d = d$data, class = d$class)))
+  plot(d$data, type = 'p', col = d$class, asp = 1)
+  plot3d(d$data, col = d$class, size = 2, type = 's')
+  aspect3d(x = 'iso')
+}
 
 # _Alpha Multivariate -----------------------------------------------------
 
@@ -145,15 +148,15 @@ sampleDataMultiStable <- function(cf, alpha) {
   
   d <- 
     matrix(
-      rstable(cf$nSample * cf$nDimention * 2, alpha = alpha, beta = 0),
-      ncol = 4
+      rstable(cf$nSample * length(theta), alpha = alpha, beta = 0),
+      ncol = length(theta)
       ) %*%
     matrix(
       c(
         (cos(theta) * weight) / sum(abs(cos(theta) * weight)),
         (sin(theta) * weight) / sum(abs(sin(theta) * weight))
         ),
-      ncol = 2
+      ncol = cf$nDimention
       )
   
   for(i in 1:cf$nClass) {
@@ -172,11 +175,14 @@ sampleDataMultiStable <- function(cf, alpha) {
   
 }
 
-# d <- sampleDataMultiStable(cf, 1.8)
-# print(head(data.frame(d = d$data, class = d$class)))
-# plot(d$data, type = 'p', col = d$class, asp = 1)
-# plot3d(d$data, col = d$class, size = 2, type = 's')
-
+if(FALSE) {
+  d <- sampleDataMultiStable(cf, 1.8)
+  print(head(data.frame(d = d$data, class = d$class)))
+  plot(d$data, type = 'p', col = d$class, asp = 1)
+  plot3d(d$data, col = d$class, size = 2, type = 's')
+  aspect3d(x = 'iso')
+}
+  
 # Parameter estimation ----------------------------------------------------
 # like Hill estimator for alpha
 
@@ -196,11 +202,12 @@ randomProjection <- function(d, targetNumDimention, alpha = 2) {
 # d <- sampleDataMultiNormal(cf)
 # d$data <- randomProjection(d$data, cf$tDimention)
 # plot(d$data, type = 'p', col = d$class, asp = 1)
-
-cl <- kmeans(d$data, 2)
+cl <- kmeans(d$data, cf$nClass, nstart = 10)
+points(d$data, pch = 3, col = cl$cluster)
 
 
 
 # Goodness ----------------------------------------------------------------
+
 
 
