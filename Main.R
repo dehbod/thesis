d7a424557d21cb38676513033f9cc7bc82eef939
Siamak Dehbod <dehbod@gmail.com>
2018-09-22 01:47:06 +0330

    Adjusted Rand Index functions moved to Util from Main #Email to Dr. Mohammadpour

--- Main.R
+++ Main.R
@@ -2,212 +2,34 @@ rm(list = ls())
 library(rgl)
 library(MASS)
 library(stabledist)
+library(mclust)
 source('Util.R')
 
 # Sample data -------------------------------------------------------------
 
-# set.seed(1038)
+set.seed(10340)
 cf <- list()
 cf$nSample <- 1000
-cf$nDimention <- 2
-cf$nClass <- 2
+cf$nDimention <- 20
+cf$nClass <- 3
 cf$tDimention <- 2
 
-# _Univariate Normal ------------------------------------------------------
-
-sampleDataUniNormal <- function(cf) {
-
-  out <- dataConfiguration(cf)
-  
-  cf <- out$cf
-  dClass <- out$dClass
-  dClassMean <- out$dClassMean
-  dClassVar <- out$dClassVar
-  
-  d <- matrix(rnorm(cf$nSample * cf$nDimention), nrow = cf$nSample)
-
-  for(i in 1:cf$nClass) {
-    for(j in 1:cf$nDimention) {
-      d[(cf$classIndex[i]+1):cf$classIndex[i+1], j] <-
-        sqrt(dClassVar[i, j, j]) * d[(cf$classIndex[i]+1):cf$classIndex[i+1], j]
-    }
-    d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] <- 
-      d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] +
-      matrix(rep(dClassMean[i,], cf$classIndex[i+1] - cf$classIndex[i]), 
-             ncol = cf$nDimention,
-             byrow = TRUE)
-  }
-
-  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
-  d <- d[permuteIndex, ]
-  dClass <- dClass[permuteIndex]
-
-  return(list(data = d, class = dClass))
-  
-}
-
-if(FALSE) {
-  d <- sampleDataUniNormal(cf)
-  print(head(data.frame(d = d$data, class = d$class)))
-  plot(d$data, type = 'p', col = d$class, asp = 1)
-  plot3d(d$data, col = d$class, size = 2, type = 's')
-  aspect3d(x = 'iso')
-}
-
-# _Multivariate Normal ----------------------------------------------------
-
-sampleDataMultiNormal <- function(cf) {
-  out <- dataConfiguration(cf, multiVariate = TRUE)
-  
-  cf <- out$cf
-  dClass <- out$dClass
-  dClassMean <- out$dClassMean
-  dClassVar <- out$dClassVar
-  
-  d <- matrix(0, nrow = 0, ncol = cf$nDimention)
-  
-  for(i in 1:cf$nClass) {
-    d <- rbind(d, mvrnorm(cf$classIndex[i+1] - cf$classIndex[i],
-                          mu = out$dClassMean[i,], Sigma = out$dClassVar[i, , ])
-    )
-  }
-  
-  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
-  d <- d[permuteIndex, ]
-  dClass <- dClass[permuteIndex]
-  
-  return(list(data = d, class = dClass))
-  
-}
-
-if(FALSE) {
-  d <- sampleDataMultiNormal(cf)
-  print(head(data.frame(d = d$data, class = d$class)))
-  plot(d$data, type = 'p', col = d$class, asp = 1)
-  plot3d(d$data, col = d$class, size = 2, type = 's')
-  aspect3d(x = 'iso')
-}
-
-# _Alpha Univariate -------------------------------------------------------
-
-sampleDataUniStable <- function(cf, alpha) {
-  
-  out <- dataConfiguration(cf)
-  
-  cf <- out$cf
-  dClass <- out$dClass
-  dClassMean <- out$dClassMean
-  dClassVar <- out$dClassVar
-  
-  d <- matrix(rstable(cf$nSample * cf$nDimention, alpha = alpha, beta = 0),
-              nrow = cf$nSample)
-  
-  for(i in 1:cf$nClass) {
-    for(j in 1:cf$nDimention) {
-      d[(cf$classIndex[i]+1):cf$classIndex[i+1], j] <-
-        sqrt(dClassVar[i, j, j]) * d[(cf$classIndex[i]+1):cf$classIndex[i+1], j]
-    }
-    d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] <- 
-      d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] +
-      matrix(rep(dClassMean[i,], cf$classIndex[i+1] - cf$classIndex[i]), 
-             ncol = cf$nDimention,
-             byrow = TRUE)
-  }
-  
-  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
-  d <- d[permuteIndex, ]
-  dClass <- dClass[permuteIndex]
-  
-  return(list(data = d, class = dClass))
-  
-}
-
-if(FALSE) {
-  d <- sampleDataUniStable(cf, 1.8)
-  print(head(data.frame(d = d$data, class = d$class)))
-  plot(d$data, type = 'p', col = d$class, asp = 1)
-  plot3d(d$data, col = d$class, size = 2, type = 's')
-  aspect3d(x = 'iso')
-}
-
-# _Alpha Multivariate -----------------------------------------------------
-
-sampleDataMultiStable <- function(cf, alpha) {
-  
-  out <- dataConfiguration(cf)
-  
-  cf <- out$cf
-  dClass <- out$dClass
-  dClassMean <- out$dClassMean
-  dClassVar <- out$dClassVar
-  
-  cf$nDimention <- 2
-
-  theta <- c(0, pi/4, pi/2, 3 * pi / 4)
-  weight <- c(1,2,1,1)
-  
-  d <- 
-    matrix(
-      rstable(cf$nSample * length(theta), alpha = alpha, beta = 0),
-      ncol = length(theta)
-      ) %*%
-    matrix(
-      c(
-        (cos(theta) * weight) / sum(abs(cos(theta) * weight)),
-        (sin(theta) * weight) / sum(abs(sin(theta) * weight))
-        ),
-      ncol = cf$nDimention
-      )
-  
-  for(i in 1:cf$nClass) {
-    d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] <- 
-      d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] +
-      matrix(rep(dClassMean[i,], cf$classIndex[i+1] - cf$classIndex[i]), 
-             ncol = cf$nDimention,
-             byrow = TRUE)
-  }
-  
-  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
-  d <- d[permuteIndex, ]
-  dClass <- dClass[permuteIndex]
-  
-  return(list(data = d, class = dClass))
-  
-}
-
-if(FALSE) {
-  d <- sampleDataMultiStable(cf, 1.8)
-  print(head(data.frame(d = d$data, class = d$class)))
-  plot(d$data, type = 'p', col = d$class, asp = 1)
-  plot3d(d$data, col = d$class, size = 2, type = 's')
-  aspect3d(x = 'iso')
-}
-  
 # Parameter estimation ----------------------------------------------------
 # like Hill estimator for alpha
 
-# Dimantion Reduction -----------------------------------------------------
-
-randomProjection <- function(d, targetNumDimention, alpha = 2) {
-  
-  pm <- matrix(rstable(ncol(d) * targetNumDimention, alpha = alpha, beta = 0),
-              nrow = ncol(d))
-  
-  d %*% pm
-  
-}
-
 # Clustering --------------------------------------------------------------
 
+# d <- sampleDataUniNormal(cf)
 # d <- sampleDataMultiNormal(cf)
-# d$data <- randomProjection(d$data, cf$tDimention)
-# plot(d$data, type = 'p', col = d$class, asp = 1)
+d <- sampleDataUniStable(cf, 1.8)
+# d <- sampleDataMultiStable(cf, 1.8)
+
+d$data <- randomProjection(d$data, cf$tDimention, alpha = 1.8)
+plot(d$data, type = 'p', col = d$class, asp = 1)
 cl <- kmeans(d$data, cf$nClass, nstart = 10)
 points(d$data, pch = 3, col = cl$cluster)
 
+# Index ----------------------------------------------------------------
 
-
-# Goodness ----------------------------------------------------------------
-
-
+print(adjustedRandIndex(d$class, cl$cluster))
 
--- Util.R
+++ Util.R
@@ -1,3 +1,191 @@
+# Sample Data Generation Functions ----------------------------------------
+
+# _Univariate Normal ------------------------------------------------------
+
+sampleDataUniNormal <- function(cf) {
+  
+  out <- dataConfiguration(cf)
+  
+  cf <- out$cf
+  dClass <- out$dClass
+  dClassMean <- out$dClassMean
+  dClassVar <- out$dClassVar
+  
+  d <- matrix(rnorm(cf$nSample * cf$nDimention), nrow = cf$nSample)
+  
+  for(i in 1:cf$nClass) {
+    for(j in 1:cf$nDimention) {
+      d[(cf$classIndex[i]+1):cf$classIndex[i+1], j] <-
+        sqrt(dClassVar[i, j, j]) * d[(cf$classIndex[i]+1):cf$classIndex[i+1], j]
+    }
+    d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] <- 
+      d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] +
+      matrix(rep(dClassMean[i,], cf$classIndex[i+1] - cf$classIndex[i]), 
+             ncol = cf$nDimention,
+             byrow = TRUE)
+  }
+  
+  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
+  d <- d[permuteIndex, ]
+  dClass <- dClass[permuteIndex]
+  
+  return(list(data = d, class = dClass))
+  
+}
+
+if(FALSE) {
+  d <- sampleDataUniNormal(cf)
+  print(head(data.frame(d = d$data, class = d$class)))
+  plot(d$data, type = 'p', col = d$class, asp = 1)
+  plot3d(d$data, col = d$class, size = 2, type = 's')
+  aspect3d(x = 'iso')
+}
+
+# _Multivariate Normal ----------------------------------------------------
+
+sampleDataMultiNormal <- function(cf) {
+  out <- dataConfiguration(cf, multiVariate = TRUE)
+  
+  cf <- out$cf
+  dClass <- out$dClass
+  dClassMean <- out$dClassMean
+  dClassVar <- out$dClassVar
+  
+  d <- matrix(0, nrow = 0, ncol = cf$nDimention)
+  
+  for(i in 1:cf$nClass) {
+    d <- rbind(d, mvrnorm(cf$classIndex[i+1] - cf$classIndex[i],
+                          mu = out$dClassMean[i,], Sigma = out$dClassVar[i, , ])
+    )
+  }
+  
+  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
+  d <- d[permuteIndex, ]
+  dClass <- dClass[permuteIndex]
+  
+  return(list(data = d, class = dClass))
+  
+}
+
+if(FALSE) {
+  d <- sampleDataMultiNormal(cf)
+  print(head(data.frame(d = d$data, class = d$class)))
+  plot(d$data, type = 'p', col = d$class, asp = 1)
+  plot3d(d$data, col = d$class, size = 2, type = 's')
+  aspect3d(x = 'iso')
+}
+
+# _Alpha Univariate -------------------------------------------------------
+
+sampleDataUniStable <- function(cf, alpha) {
+  
+  out <- dataConfiguration(cf)
+  
+  cf <- out$cf
+  dClass <- out$dClass
+  dClassMean <- out$dClassMean
+  dClassVar <- out$dClassVar
+  
+  d <- matrix(rstable(cf$nSample * cf$nDimention, alpha = alpha, beta = 0),
+              nrow = cf$nSample)
+  
+  for(i in 1:cf$nClass) {
+    for(j in 1:cf$nDimention) {
+      d[(cf$classIndex[i]+1):cf$classIndex[i+1], j] <-
+        sqrt(dClassVar[i, j, j]) * d[(cf$classIndex[i]+1):cf$classIndex[i+1], j]
+    }
+    d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] <- 
+      d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] +
+      matrix(rep(dClassMean[i,], cf$classIndex[i+1] - cf$classIndex[i]), 
+             ncol = cf$nDimention,
+             byrow = TRUE)
+  }
+  
+  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
+  d <- d[permuteIndex, ]
+  dClass <- dClass[permuteIndex]
+  
+  return(list(data = d, class = dClass))
+  
+}
+
+if(FALSE) {
+  d <- sampleDataUniStable(cf, 1.8)
+  print(head(data.frame(d = d$data, class = d$class)))
+  plot(d$data, type = 'p', col = d$class, asp = 1)
+  plot3d(d$data, col = d$class, size = 2, type = 's')
+  aspect3d(x = 'iso')
+}
+
+# _Alpha Multivariate -----------------------------------------------------
+
+sampleDataMultiStable <- function(cf, alpha) {
+  
+  out <- dataConfiguration(cf)
+  
+  cf <- out$cf
+  dClass <- out$dClass
+  dClassMean <- out$dClassMean
+  dClassVar <- out$dClassVar
+  
+  cf$nDimention <- 2
+  
+  theta <- c(0, pi/4, pi/2, 3 * pi / 4)
+  weight <- c(1,2,1,1)
+  
+  d <- 
+    matrix(
+      rstable(cf$nSample * length(theta), alpha = alpha, beta = 0),
+      ncol = length(theta)
+    ) %*%
+    matrix(
+      c(
+        (cos(theta) * weight) / sum(abs(cos(theta) * weight)),
+        (sin(theta) * weight) / sum(abs(sin(theta) * weight))
+      ),
+      ncol = cf$nDimention
+    )
+  
+  for(i in 1:cf$nClass) {
+    d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] <- 
+      d[(cf$classIndex[i]+1):cf$classIndex[i+1], ] +
+      matrix(rep(dClassMean[i,], cf$classIndex[i+1] - cf$classIndex[i]), 
+             ncol = cf$nDimention,
+             byrow = TRUE)
+  }
+  
+  permuteIndex <- sample.int(cf$nSample, size = cf$nSample)
+  d <- d[permuteIndex, ]
+  dClass <- dClass[permuteIndex]
+  
+  return(list(data = d, class = dClass))
+  
+}
+
+if(FALSE) {
+  d <- sampleDataMultiStable(cf, 1.8)
+  print(head(data.frame(d = d$data, class = d$class)))
+  plot(d$data, type = 'p', col = d$class, asp = 1)
+  plot3d(d$data, col = d$class, size = 2, type = 's')
+  aspect3d(x = 'iso')
+}
+
+
+
+
+# Dimention Reduction -----------------------------------------------------
+
+randomProjection <- function(d, targetNumDimention, alpha = 2) {
+  
+  pm <- matrix(rstable(ncol(d) * targetNumDimention, alpha = alpha, beta = 0),
+               nrow = ncol(d))
+  
+  d %*% pm
+  
+}
+
+# Util Functions ----------------------------------------------------------
+
 dataConfiguration <- function(cf, multiVariate = FALSE) {
   
   if(!('classIndex' %in% names(cf))) {
